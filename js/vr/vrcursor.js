73567110ed9d331e948b0ec6e2a570dab6c3809f
Andrea Hawksley <hawksley@gmail.com>
2014-11-18 16:10:30 -0800

    experiment here

---
+++ js/vr/vrcursor.js
@@ -0,0 +1,429 @@
+function VRCursor(mode) {
+  var self = this;
+
+  this.enabled = false;
+
+  // current scene context in which the cursor is operating
+  this.context = null;
+
+  // Object 3d for cursor
+  this.layout = null;
+
+  // system mouse vector
+  this.mouse = null;
+
+  // body el
+  this.body = window && window.parent? window.parent.document.body : document.body;
+
+  this.rotation = {
+    x: 0,
+    y: 0,
+    xInc: 0,
+    yInc: 0
+  };
+
+  // VR cursor position
+  this.position = {
+    x: 0,
+    y: 0
+  }
+
+  // object which cursor currently intersects
+  this.objectMouseOver = null;
+
+  // cursor operating mode
+  this.mode = mode || 'centered';
+
+  this.setMode(this.mode);
+
+
+  //  return promise when all the necessary three cursor components are ready.
+  this.ready = new Promise(function(resolve) {
+      self.layout = new THREE.Group();
+      self.raycaster = new THREE.Raycaster();
+      self.cursorPivot = new THREE.Object3D();
+      self.projector = new THREE.Projector();
+
+      // cursor (line)
+      var material = new THREE.LineBasicMaterial({color: 0xffffff, linewidth: 0});
+      var geometry = new THREE.CircleGeometry( 0.0, 0 );
+      geometry.vertices.shift(); // remove center vertex
+      self.cursor = new THREE.Line( geometry, material );
+
+      self.layout.visible = self.enabled;
+
+      // set the depth of cursor
+      self.cursor.position.z = -2;
+
+      self.cursorPivot.add(self.cursor);
+
+      self.layout.add(self.cursorPivot);
+
+      resolve();
+  });
+
+  // bind "real" mouse events.
+  if (this.enabled) {
+    this.bindEvents();
+  }
+}
+
+VRCursor.modes = VRCursor.prototype.modes = {
+  centered: 1,
+  mouseSync: 2,
+  inFOV: 3,
+  hides: 4,
+  mono: 5
+};
+
+VRCursor.prototype.setMode = function(mode) {
+  console.log('setting cursor: ' + mode);
+
+  this.mode = this.modes[mode];
+
+  switch (this.mode) {
+    case this.modes.centered:
+      this.updatePosition = this.updatePositionCentered;
+      break;
+    case this.modes.mouseSync:
+      this.updatePosition = this.updatePositionMouseSync;
+      break;
+    case this.modes.inFOV:
+      this.updatePosition = this.updatePositionInFOV;
+      break;
+    case this.modes.hides:
+      this.updatePosition = this.updatePositionHides;
+      break;
+    case this.modes.mono:
+      this.updatePosition = function() {};
+      break;
+  }
+}
+
+// enable cursor with context, otherwise pick last
+VRCursor.prototype.enable = function(context) {
+  if (context) {
+    this.context = context;
+  };
+
+  if (!this.enabled) {
+    this.bindEvents();
+    this.enabled = true;
+    this.layout.visible = true;
+
+    // keeping raycaster separate of main frame loop update function keeps performance in check.
+    this.raycastIntervalId = window.setInterval(this.rayLoop.bind(this), 50);
+  };
+};
+
+VRCursor.prototype.rayLoop = function() {
+  if (this.enabled) {
+    this.updateCursorIntersection();
+  } else if (this.raycastIntervalId) {
+    clearInterval(this.raycastIntervalId);
+  }
+}
+VRCursor.prototype.disable = function() {
+  if (this.enabled) {
+    this.unbindEvents();
+    this.enabled = false;
+    this.layout.visible = false;
+  }
+}
+// requires three.js dom and camera to initialize cursor.
+VRCursor.prototype.init = function( dom, camera, context ) {
+  this.dom = dom;
+  this.camera = camera;
+  this.context = context;
+}
+
+// VR Cursor events
+VRCursor.prototype.events = {
+  clickEvent : { type: 'click'},
+  mouseMoveEvent : { type: 'mousemove' },
+  mouseOverEvent : { type: 'mouseover' },
+  mouseOutEvent : { type: 'mouseout' },
+  mouseDownEvent : { type: 'mousedown' }
+}
+
+// binds mouse events
+VRCursor.prototype.bindEvents = function() {
+  var body = this.body;
+  var onMouseMoved = this.onMouseMoved.bind(this);
+  var onMouseClicked = this.onMouseClicked.bind(this);
+
+  body.addEventListener("mousemove", onMouseMoved, false);
+  body.addEventListener("click", onMouseClicked, false);
+};
+
+VRCursor.prototype.unbindEvents = function() {
+  var body = this.body;
+
+  body.removeEventListener("mousemove", this.onMouseMoved, false);
+  body.removeEventListener("click", this.onMouseClicked, false);
+};
+
+VRCursor.prototype.clampAngleTo = function(angle, boundary) {
+  if (angle < -boundary) {
+    return -boundary;
+  }
+  if (angle > boundary) {
+    return boundary;
+  }
+  return angle;
+};
+
+VRCursor.prototype.hide = function(delay, hidden) {
+  var self = this;
+  clearTimeout(this.hideCursorTimeout);
+  this.hideCursorTimeout = setTimeout(hideCursor, delay);
+  function hideCursor() {
+    self.cursor.visible = false;
+    if (hidden) { hidden(); }
+  }
+};
+
+VRCursor.prototype.show = function() {
+  this.cursor.visible = true;
+};
+
+VRCursor.prototype.headQuat = function() {
+  var headQuat = this.camera.quaternion;
+  // // If head quaternion is null we make the identity so
+  // it doesn't affect the rotation composition
+  if (headQuat.x === 0 &&
+      headQuat.y === 0 &&
+      headQuat.z === 0) {
+    headQuat.w = 1;
+  }
+  return headQuat;
+};
+
+// VR Cursor events
+VRCursor.prototype.onMouseMoved = function(e) {
+  e.preventDefault();
+
+  if (!this.enabled) {
+    return false;
+  }
+
+  // get vectors for 2d mono mouse
+  if (this.mode == this.modes.mono) {
+    var mouse = new THREE.Vector3( ( e.clientX / window.innerWidth ) * 2 - 1,   //x
+      -( e.clientY / window.innerHeight ) * 2 + 1,  //y
+      0.5 );
+    mouse.unproject(this.camera)
+    mouse.sub(this.camera.position);
+    mouse.normalize();
+    this.mouse = mouse;
+  }
+
+  // everything below assumes pointerlock
+  if (this.mode === this.modes.hides) {
+    if (!this.cursor.visible) {
+      this.showQuat = new THREE.Quaternion().copy(this.headQuat());
+      this.rotation.x = 0;
+      this.rotation.y = 0;
+      this.show();
+    }
+    this.hide(3000);
+  }
+
+  var movementX = e.movementX ||
+      e.mozMovementX ||
+      e.webkitMovementX || 0;
+
+  var movementY = e.movementY ||
+      e.mozMovementY ||
+      e.webkitMovementY || 0;
+
+  var pixelsToDegreesFactor = 0.00025;
+
+  // Rotation in degrees
+  var x = (movementX * pixelsToDegreesFactor) % 360;
+  var y = (movementY * pixelsToDegreesFactor) % 360;
+
+  // To Radians
+  this.rotation.x -= y * 2 * Math.PI;
+  this.rotation.y -= x * 2 * Math.PI;
+
+};
+
+VRCursor.prototype.onMouseClicked = function(e) {
+  var target = e.target;
+
+  if (!this.enabled) {
+    return false;
+  };
+
+  if (target.tagName == 'BODY' ||
+    target.tagName == 'CANVAS') {
+
+    if (this.objectMouseOver) {
+      this.objectMouseOver.dispatchEvent(this.events.clickEvent);
+    }
+
+  };
+}
+
+VRCursor.prototype.updatePositionMouseSync = function(headQuat) {
+  var headQuat = this.headQuat();
+  this.rotation.x = this.clampAngleTo(this.rotation.x, Math.PI / 6);
+  this.rotation.y = this.clampAngleTo(this.rotation.y, Math.PI / 6);
+  var rotation = new THREE.Euler(this.rotation.x, this.rotation.y, 0);
+  var mouseQuat = new THREE.Quaternion().setFromEuler(rotation, true);
+  var cursorPivot = this.cursorPivot;
+  var pivotQuat = new THREE.Quaternion();
+  pivotQuat
+    .multiply(headQuat)
+    .multiply(mouseQuat)
+    .normalize();
+  cursorPivot.setRotationFromQuaternion(pivotQuat);
+};
+
+VRCursor.prototype.updatePositionCentered = function(headQuat) {
+  var headQuat = this.headQuat();
+  var cursorPivot = this.cursorPivot;
+  var pivotQuat = new THREE.Quaternion();
+  pivotQuat.multiply(headQuat);
+  cursorPivot.setRotationFromQuaternion(pivotQuat);
+  cursorPivot.position.copy( this.camera.position )
+};
+
+VRCursor.prototype.updatePositionInFOV = function(headQuat) {
+  var headQuat = this.headQuat();
+  var headQuatInv = new THREE.Quaternion().copy(headQuat).inverse();
+  var cursorPivot = this.cursorPivot;
+  var pivotQuat = new THREE.Quaternion();
+  var mouseQuat = this.mouseQuat();
+  if (this.cameraCursorAngle >= (Math.PI / 5)) {
+    this.lockQuat = this.lockQuat || this.validCameraCursorQuat;
+    pivotQuat.multiply(headQuat).normalize().multiply(this.lockQuat);
+  } else {
+    this.lockQuat = undefined;
+    pivotQuat.multiply(headQuat).multiply(mouseQuat).multiply(headQuatInv);
+  }
+
+  cursorPivot.quaternion.copy(pivotQuat);
+  this.validCameraCursorQuat = this.cameraCursorQuat(cursorPivot.quaternion || new THREE.Quaternion());
+  this.cameraCursorAngle = this.quaternionsAngle(headQuat, cursorPivot.quaternion);
+};
+
+VRCursor.prototype.mouseQuat = function() {
+  var rotation = new THREE.Euler(this.rotation.x, this.rotation.y, 0);
+  return new THREE.Quaternion().setFromEuler(rotation, true);
+};
+
+VRCursor.prototype.updatePositionHides = function(headQuat) {
+  var self = this;
+  var rotation = new THREE.Euler(this.rotation.x, this.rotation.y, 0);
+  var mouseQuat = new THREE.Quaternion().setFromEuler(rotation, true);
+  var cursorPivot = this.cursorPivot;
+  var headQuat = this.headQuat();
+  var pivotQuat = new THREE.Quaternion();
+  var showQuat = this.showQuat || new THREE.Quaternion();
+  var cameraCursorAngle;
+  pivotQuat.multiply(showQuat);
+  pivotQuat.multiply(mouseQuat).normalize();
+  cursorPivot.quaternion.copy(pivotQuat);
+  cameraCursorAngle = this.quaternionsAngle(headQuat, cursorPivot.quaternion);
+  // if the cursor lives the FOV it hides
+  if (cameraCursorAngle >= Math.PI / 3) {
+    this.hide(0);
+  }
+};
+
+VRCursor.prototype.update = function(headQuat) {
+  this.updatePosition();
+  // It updates hits
+  // this.updateCursorIntersection();
+};
+
+VRCursor.prototype.quaternionsAngle = function(q1, q2) {
+  var v1 = new THREE.Vector3( 0, 0, -1 );
+  var v2 = new THREE.Vector3( 0, 0, -1 );
+  v1.applyQuaternion(q1);
+  v2.applyQuaternion(q2);
+  return Math.abs(v1.angleTo(v2));
+};
+
+VRCursor.prototype.quaternionsQuat = function(q1, q2) {
+  var v1 = new THREE.Vector3( 0, 0, -1 );
+  var v2 = new THREE.Vector3( 0, 0, -1 );
+  v1.applyQuaternion(q1);
+  v2.applyQuaternion(q2);
+  return new THREE.Quaternion().setFromUnitVectors( v1, v2 );
+};
+
+VRCursor.prototype.cameraCursorQuat = function(mouseQuat) {
+  var cameraQuat = this.headQuat();
+  var cameraVector = new THREE.Vector3( 0, 0, -1 );
+  var cursorPivotVector = new THREE.Vector3( 0, 0, -1 );
+  cameraVector.applyQuaternion(cameraQuat).normalize();
+  cursorPivotVector.applyQuaternion(mouseQuat).normalize();
+  return new THREE.Quaternion().setFromUnitVectors( cameraVector, cursorPivotVector );
+};
+
+VRCursor.prototype.cameraCursorAngle = function() {
+  var cameraQuat = this.headQuat();
+  var cameraVector = new THREE.Vector3( 0, 0, -1 );
+  var cursorPivot = this.cursorPivot;
+  var cursorPivotVector = new THREE.Vector3( 0, 0, -1 );
+  cameraVector.applyQuaternion(cameraQuat);
+  cursorPivotVector.applyQuaternion(cursorPivot.quaternion);
+  return Math.abs(cursorPivotVector.angleTo(cameraVector));
+};
+
+
+// Detect intersections with three.js scene objects (context) and dispatch mouseover and mouseout events.
+VRCursor.prototype.updateCursorIntersection = function() {
+  var camera = this.camera;
+  var raycaster = this.raycaster;
+  var cursor = this.cursor;
+  var mouse = this.mouse;
+
+  if (!camera) {
+    // no camera available yet.
+    return false;
+  }
+
+  if (mouse && this.mode == this.modes.mono) {
+    raycaster.set( camera.position, mouse );
+  } else {
+    var cursorPosition = cursor.matrixWorld;
+    vector = new THREE.Vector3().setFromMatrixPosition(cursorPosition);
+
+    raycaster.set( camera.position, vector.sub( camera.position ).normalize() );
+  }
+
+
+  var intersects = raycaster.intersectObjects( this.context.children );
+
+  var intersected;
+  var i;
+
+  var objectMouseOver = this.objectMouseOver;
+  var events = this.events;
+
+  if (intersects.length == 0 && objectMouseOver !== null) {
+    //this.cursor.material.color.set(0x9f2ac6);
+
+    this.objectMouseOver.dispatchEvent(events.mouseOutEvent);
+    this.objectMouseOver = null;
+  }
+
+  for (i = 0; i < intersects.length; ++i) {
+    intersected = intersects[0].object;
+
+    if (intersected !== objectMouseOver) {
+      if (objectMouseOver !== null) {
+        objectMouseOver.dispatchEvent(events.mouseOutEvent);
+      }
+      if (intersected !== null) {
+        intersected.dispatchEvent(events.mouseOverEvent);
+      }
+
+      this.objectMouseOver = intersected;
+    }
+  }
+};
---
+++ js/worldNav.js
@@ -0,0 +1,262 @@
+/**
+Add videos here.
+*/
+var videoChoices = [
+	{
+    "name": "elevr1",
+    "texture": 'images/denver-botanical-5-small.jpg',
+		"url": "http://mozvr.com/content/elevr4",
+		"title": {
+			"title": "ELEVR: Talk chat show EP.4",
+  		"titleCredits": "ELEVR\n-\nWebGL"
+		},
+    "selected": false
+	},
+  {
+    "name": "elevr2",
+    "texture": 'images/kirby-cove-1-small.jpg',
+    "url": "http://vihart.github.io/webVR-playing-with/compound",
+    "title": {
+      "title": "ELEVR: Talk chat show EP.4",
+      "titleCredits": "ELEVR\n-\nWebGL"
+    },
+    "selected": false
+  },
+  {
+    "name": "elevr3",
+    "texture": 'images/mosaic-math-art.JPG',
+    "url": "http://vihart.github.io/webVR-playing-with/inside",
+    "title": {
+      "title": "ELEVR: Talk chat show EP.4",
+      "titleCredits": "ELEVR\n-\nWebGL"
+    },
+    "selected": false
+  },
+  {
+    "name": "elevr4",
+    "texture": 'images/utrecht-canal.jpg',
+    "url": "http://emilyeifler.github.io/this-is-my-horse/horse.html",
+    "title": {
+      "title": "ELEVR: Talk chat show EP.4",
+      "titleCredits": "ELEVR\n-\nWebGL"
+    },
+    "selected": false
+  }
+];
+
+window.WorldNav = (function() {
+  function WorldNav() {
+    var self = this;
+
+    self.container = document.querySelector("#container");
+    self.container.height = window.innerHeight;
+    self.container.width = window.innerWidth;
+
+    self.navContainer = document.querySelector("#nav");
+
+    /*
+    Setup three.js WebGL renderer
+    */
+    self.renderer = new THREE.WebGLRenderer( { antialias: true } );
+    self.navContainer.appendChild(self.renderer.domElement);
+
+    /*
+    Create a three.js scene
+    */
+    self.scene = new THREE.Scene();
+
+    /*
+    Create a three.js camera
+    */
+    self.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+
+    self.cursor = new VRCursor();
+    self.cursor.ready.then(function() {
+    	self.scene.add(self.cursor.layout);
+    	self.cursor.init(self.renderer.domElement, self.camera, self.scene);
+    	self.cursor.enable();
+    });
+
+    /*
+    Apply VR headset positional data to camera.
+    */
+    self.controls = new THREE.VRControls( self.camera );
+
+    /*
+    Apply VR stereo rendering to renderer
+    */
+    self.effect = new THREE.VREffect( self.renderer );
+    self.effect.setSize( window.innerWidth, window.innerHeight );
+
+    /*
+    Create 3d objects
+    */
+    var backgroundGeometry   = new THREE.SphereGeometry(200, 32, 32);
+    var backgroundMaterial  = new THREE.MeshBasicMaterial();
+    backgroundMaterial.map = THREE.ImageUtils.loadTexture('images/video1.jpg');
+    var backgroundMesh = new THREE.Mesh(backgroundGeometry, backgroundMaterial);
+    backgroundMesh.scale.x = -1;
+    self.scene.add(backgroundMesh);
+
+    var planetGeometry   = new THREE.SphereGeometry(5, 32, 32);
+
+    for (var i=0; i<videoChoices.length; i++) {
+      var pi = 3.1415926535897932384626;
+      var planetMaterial  = new THREE.MeshBasicMaterial( );
+      planetMaterial.map = THREE.ImageUtils.loadTexture(videoChoices[i].texture);
+      planetMaterial.color = {r:0.5, g:0.5, b:0.5};
+      videoChoices[i].planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
+      var z = Math.sin(-3/2*pi/videoChoices.length*i)*20;
+      var x = Math.cos(-3/2*pi/videoChoices.length*i)*20;
+      videoChoices[i].planetMesh.position.z = z;
+      videoChoices[i].planetMesh.position.x = x;
+			videoChoices[i].planetMesh.scale.x = -1;
+      self.scene.add( videoChoices[i].planetMesh );
+    }
+
+		self.selectedPlanet = videoChoices[0];
+		self.frameCount = 0;
+		self.growWorld = function(selectedPlanet) {
+			self.selectedPlanet = selectedPlanet;
+			for (var i=0; i<videoChoices.length; i++) {
+				if (self.selectedPlanet !== videoChoices[i]) {
+						self.scene.remove(videoChoices[i].planetMesh); //remove back other planets
+				}
+			}
+			self.frameCount = 0;
+		}
+
+		self.resetPlanets = function() {
+			self.selectedPlanet.planetMesh.scale.set(-1,1,1);
+			for (var i=0; i<videoChoices.length; i++) {
+				self.scene.add(videoChoices[i].planetMesh); //add back other planets
+			}
+		}
+
+    self.animate = function() {
+      /*
+      Update VR headset position and apply to camera and cursor.
+      */
+      self.controls.update();
+      if (self.cursor.enabled) {
+        self.cursor.updatePosition();
+        for (var i=0; i<videoChoices.length; i++) {
+          if (self.cursor.objectMouseOver === videoChoices[i].planetMesh) {
+            //make brighter, TODO: something nicer later?
+            if (!videoChoices[i].selected) {
+              videoChoices[i].selected = true;
+              videoChoices[i].planetMesh.material.color = {r:1, g:1, b:1};
+            }
+            videoChoices[i].planetMesh.rotation.y += 0.02;
+          } else {
+            //make darker
+            if (videoChoices[i].selected) {
+              videoChoices[i].selected = false;
+              videoChoices[i].planetMesh.material.color = {r:0.5, g:0.5, b:0.5};
+            }
+            videoChoices[i].planetMesh.rotation.y += 0.01;
+          }
+        }
+      } else if (self.frameCount < 125) { //experimental transition - would make more sense for a picture viewer...
+				self.selectedPlanet.planetMesh.scale.x += -0.04;
+				self.selectedPlanet.planetMesh.scale.y += 0.04;
+				self.selectedPlanet.planetMesh.scale.z += 0.04;
+				self.frameCount++;
+			} else if (self.frameCount === 125) { //hide and transition. only need to do this once
+				self.navContainer.style.display = "none";
+				self.frameCount++;
+			}
+
+      /*
+      Render the scene through the VREffect.
+      */
+      self.effect.render( self.scene, self.camera );
+
+      requestAnimationFrame( self.animate );
+    }
+  }
+
+  return new WorldNav();
+})();
+
+
+/*
+Kick off animation loop
+*/
+WorldNav.animate();
+
+function fullScreen() {
+  var fullDiv = document.getElementById('full');
+  if (fullDiv.mozRequestFullScreen) {
+    fullDiv.mozRequestFullScreen({ vrDisplay: WorldNav.effect._vrHMD }); // Firefox
+  } else if (fullDiv.webkitRequestFullscreen) {
+    fullDiv.webkitRequestFullscreen({ vrDisplay: WorldNav.effect._vrHMD }); // Chrome and Safari
+  } else if (fullDiv.requestFullScreen){
+    fullDiv.requestFullscreen();
+  }
+}
+
+/*
+Listen for double click event to enter full-screen VR mode
+*/
+document.body.addEventListener( 'dblclick', function() {
+  fullScreen();
+});
+
+/*
+Listen for keyboard event and zero positional sensor on appropriate keypress.
+*/
+function onkey(event) {
+  event.preventDefault();
+
+  if (event.keyCode == 90) { // z to zero sensor
+    WorldNav.controls.zeroSensor();
+  } else if (event.keyCode == 70) { // f to fullscreen
+    fullScreen();
+  } else if (event.keyCode == 32) { // spacebar to select planet
+    if (WorldNav.cursor.enabled) {
+      WorldNav.cursor.updatePosition();
+      for (var i=0; i<videoChoices.length; i++) {
+        if (WorldNav.cursor.objectMouseOver === videoChoices[i].planetMesh) {
+          // enter scene
+          WorldNav.container.src = videoChoices[i].url;
+					WorldNav.cursor.disable();
+					WorldNav.growWorld(videoChoices[i]);
+        }
+      }
+    } else { // Cursor is disabled when not in nav, return to nav
+			WorldNav.resetPlanets();
+      WorldNav.container.src = "";
+      WorldNav.navContainer.style.display = "block";
+      WorldNav.cursor.enable();
+    }
+  }
+};
+window.addEventListener("keydown", onkey, true);
+
+function key(event, sign) {
+  var control = WorldNav.controls.manualControls[String.fromCharCode(event.keyCode).toLowerCase()];
+  if (!control)
+    return;
+  if (sign === 1 && control.active || sign === -1 && !control.active)
+    return;
+  control.active = (sign === 1);
+  WorldNav.controls.manualRotateRate[control.index] += sign * control.sign;
+}
+document.addEventListener('keydown', function(event) { key(event, 1); }, false);
+document.addEventListener('keyup', function(event) { key(event, -1); }, false);
+
+/*
+Handle window resizes
+*/
+function onWindowResize() {
+  WorldNav.camera.aspect = window.innerWidth / window.innerHeight;
+  WorldNav.camera.updateProjectionMatrix();
+
+  WorldNav.effect.setSize( window.innerWidth, window.innerHeight );
+
+  WorldNav.container.height = window.innerHeight;
+  WorldNav.container.width = window.innerWidth;
+}
+
+window.addEventListener( 'resize', onWindowResize, false );
